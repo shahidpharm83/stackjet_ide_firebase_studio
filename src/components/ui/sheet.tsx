a173106e64da448fcb9024852192c6f295565264
Shahid Islam <shahidpharna1983@gmail.com>
2025-08-01 04:05:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
+
 
 import { cn } from "@/lib/utils"
 
@@ -69,6 +71,9 @@ const SheetContent = React.forwardRef<
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
+       <VisuallyHidden.Root>
+        <SheetPrimitive.Title>Sheet</SheetPrimitive.Title>
+      </VisuallyHidden.Root>
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
